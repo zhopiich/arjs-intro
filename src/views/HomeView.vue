c29c11629de38ee4ed3704cb7abd9694822b9d3b
zhopiich <zhoping9449@gmail.com>
2026-05-22 22:53:46 +0800

    feat: add HomeView and integrate ArSceneCanvas component

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ArSceneCanvas from '@/components/ar-viewer/ArSceneCanvas.vue'
+</script>
+
+<template>
+  <main class="home-view">
+    <ArSceneCanvas />
+  </main>
+</template>
+
+<style scoped>
+.home-view {
+  min-height: 100vh;
+  background: #111;
+  color: #fff;
+}
+</style>
