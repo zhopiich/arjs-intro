2b3dabde83c039cfbab7cc3f15cd5fd20b905554
zhopiich <zhoping9449@gmail.com>
2026-05-23 21:36:29 +0800

    feat(navigation): integrate global stage switcher navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import StageNav from '@/components/StageNav.vue'
 </script>
 
 <template>
+  <StageNav />
   <RouterView />
 </template>
